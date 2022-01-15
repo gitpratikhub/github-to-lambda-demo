578b44ec923391d855f2bb9c2b34e246d4f43046
pratik <pratik.mane@outlook.com>
2022-01-15 15:24:22 +0530

    v1.3

--- lambda_function.py
+++ lambda_function.py
@@ -1,6 +1,6 @@
 import pandas as pd
 def lambda_handler(event, context):
-    d = {'col1':[1,2],'col2':[3,4]}
+    d = {'col1':[1,2,3,4,5,6],'col2':[11,22,33,44,55,66]}
     df = pd.DataFrame(data = d)
     print (df)
-    print ('done V1.0-omkar')
+    print ('done V1.3')
