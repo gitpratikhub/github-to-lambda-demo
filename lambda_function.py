b4a081fab18622b9c0331284a8da80ebdcb21266
gitpratikhub <pratik.mane@outlook.com>
2022-01-20 11:11:13 +0530

    comit from ubuntu

--- lambda_function.py
+++ lambda_function.py
@@ -3,4 +3,4 @@ def lambda_handler(event, context):
     d = {'col1':[1,2,3,4,5,6],'col2':[11,22,33,44,55,66]}
     df = pd.DataFrame(data = d)
     print (df)
-    print ('done V1.3')
+    print ('done V1.4')
